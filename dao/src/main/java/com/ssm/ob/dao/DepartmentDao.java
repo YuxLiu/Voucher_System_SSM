811b13f38b006b3d688fd06df869385699f657d0
Ethan Liu <liuyuxuan1230@outlook.com>
2019-07-19 12:47:50 +0800

    add comment to Dao @Repository

--- dao/src/main/java/com/ssm/ob/dao/DepartmentDao.java
+++ dao/src/main/java/com/ssm/ob/dao/DepartmentDao.java
@@ -5,11 +5,18 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository// 添加("departmentDao") 会导致 biz.departmentDao bean 要求 qualify
+// 添加("departmentDao") 会导致 biz.departmentDao bean 要求 qualify
+// 原因猜想: bean 名称默认为接口名, ("alias") 是别名,
+//          2 个名称相同 则相当于: 1 个对象同时有 2 个引用
+@Repository
 public interface DepartmentDao {
     void insert(Department department);
+
     void update(Department department);
+
     void delete(String sn);
+
     Department select(String sn);
+
     List<Department> selectAll();
 }
